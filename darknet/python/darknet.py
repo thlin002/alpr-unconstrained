1a68c1c6992b74e731a8360e9912b5fbfcfe8e2e
thlin002 <62495278+thlin002@users.noreply.github.com>
2021-03-05 11:32:12 +0800

    Update darknet.py

--- darknet/python/darknet.py
+++ darknet/python/darknet.py
@@ -136,8 +136,8 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
         for i in range(meta.classes):
             if dets[j].prob[i] > 0:
                 b = dets[j].bbox
-                res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
-    res = sorted(res, key=lambda x: -x[1])
+                res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))	#(class, prob, bbx)
+    res = sorted(res, key=lambda x: -x[1])	# sorted with prob
     wh = (im.w,im.h)
     free_image(im)
     free_detections(dets, num)
